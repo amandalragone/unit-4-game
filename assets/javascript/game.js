9c81a158ac6be9442dbdba0e4bdfee809d76fcb0
Amanda <amandalragone@gmail.com>
2019-03-24 21:06:11 -0500

    Created function to initialize game automatically after the user wins or loses.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,45 @@
 $(document).ready(function(){
 
-    //Computer picks a random number between 19 and 120.
+    //We need a function that will initialize a new game after player wins or loses.
 
-    var newNumber = 19 + Math.floor(Math.random() * 102);
-    console.log(newNumber);
+    var newNumber
+    var allCrystals
+    var totalScore
+    var wins = 0;
+    $("#wins").text(wins);
+    
+    var losses = 0;
+    $("#losses").text(losses);
 
-    $("#numberSelected").text(newNumber);
+    var newGame = function() {
 
-    //4 numbers between 1 and 12 will be selected.
+        //Computer picks a random number between 19 and 120.
 
-    var allCrystals = [];
+        newNumber = 19 + Math.floor(Math.random() * 102);
+        console.log(newNumber);
 
-    for (var i = 0; i < 4; i++) {
-        var crystalValue = 1 + Math.floor(Math.random() * 12);
-        allCrystals.push(crystalValue);
-    };
+        $("#numberSelected").text(newNumber);
 
-    console.log(allCrystals);
+        //4 numbers between 1 and 12 will be selected.
+
+        allCrystals = [];
+
+        for (var i = 0; i < 4; i++) {
+            var crystalValue = 1 + Math.floor(Math.random() * 12);
+            allCrystals.push(crystalValue);
+        };
+
+        console.log(allCrystals);
+
+        totalScore = 0;
+        $("#totalScore").text(totalScore);
+
+    }
+
+    
 
     //Now, each of the crystals will be assigned with one of the numbers in the allCrystals array. Score will go up by corresponding random number when each crystal is pressed.
 
-    var totalScore = 0;
-
-    var wins = 0;
-    $("#wins").text(wins);
-
-    var losses = 0;
-    $("#losses").text(losses);
 
     $("#red_crystal").on("click", function() {
         totalScore = totalScore + allCrystals[0];
@@ -38,11 +51,13 @@ $(document).ready(function(){
         if (totalScore === newNumber) {
             wins = wins + 1;
             $("#wins").text(wins);
-            $("#win-lose-msg").text("Congratulations! You've reached the correct number! We're setting your wins to " + wins + "! Let's play again.");
+            $("#win-lose-msg").text("Congratulations! You've reached the correct number! We've set your wins to " + wins + "! Let's play again.");
+            newGame();
         } else if (totalScore > newNumber) {
             losses = losses + 1;
             $("#losses").text(losses);
             $("#win-lose-msg").text("Oh oh, you lost... The number selected was " + newNumber + "... But it's ok. Let's try again now.");
+            newGame();
         };
     });
 
@@ -57,10 +72,12 @@ $(document).ready(function(){
             wins = wins + 1;
             $("#wins").text(wins);
             $("#win-lose-msg").text("Congratulations! You've reached the correct number! We're setting your wins to " + wins + "! Let's play again.");
+            newGame();
         } else if (totalScore > newNumber) {
             losses = losses + 1;
             $("#losses").text(losses);
             $("#win-lose-msg").text("Oh oh, you lost... The number selected was " + newNumber + "... But it's ok. Let's try again now.");
+            newGame();
         };
     });
 
@@ -75,10 +92,12 @@ $(document).ready(function(){
             wins = wins + 1;
             $("#wins").text(wins);
             $("#win-lose-msg").text("Congratulations! You've reached the correct number! We're setting your wins to " + wins + "! Let's play again.");
+            newGame();
         } else if (totalScore > newNumber) {
             losses = losses + 1;
             $("#losses").text(losses);
             $("#win-lose-msg").text("Oh oh, you lost... The number selected was " + newNumber + "... But it's ok. Let's try again now.");
+            newGame();
         };
     });
 
@@ -93,19 +112,16 @@ $(document).ready(function(){
             wins = wins + 1;
             $("#wins").text(wins);
             $("#win-lose-msg").text("Congratulations! You've reached the correct number! We're setting your wins to " + wins + "! Let's play again.");
+            newGame();
         } else if (totalScore > newNumber) {
             losses = losses + 1;
             $("#losses").text(losses);
             $("#win-lose-msg").text("Oh oh, you lost... The number selected was " + newNumber + "... But it's ok. Let's try again now.");
+            newGame();
         };
 
     });
-
-    
-    
-
-
-
+    newGame();
 
 
 })
